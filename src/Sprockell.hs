989cf37b218dc2155f9176772e47668eba5990c8
Leon Schoorl <l.m.schoorl@student.utwente.nl>
2017-06-07 01:05:17 +0200

    export Sprockell.System from Sprockell

--- src/Sprockell.hs
+++ src/Sprockell.hs
@@ -3,6 +3,7 @@ module Sprockell
     , module Sprockell.BasicFunctions
     , module Sprockell.Simulation
     , module Sprockell.Debugger
+    , module Sprockell.System
     ) where
 
 import Sprockell.HardwareTypes hiding (regX,regY) -- hide regX,Y to avoid confusion with regA-E
