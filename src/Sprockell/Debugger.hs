a2e27d600adf2425dbe6ace9ed36642f47f2c808
Leon Schoorl <l.m.schoorl@student.utwente.nl>
2017-06-08 01:10:05 +0200

    Generalize debuggers
    debuggerPrintCondWaitCond is a generalization
    of both debuggerSimplePrint and debuggerSimplePrintAndWait

--- src/Sprockell/Debugger.hs
+++ src/Sprockell/Debugger.hs
@@ -1,5 +1,6 @@
 module Sprockell.Debugger where
 import Sprockell.HardwareTypes
+import Control.Monad (when, void)
 
 -- | A DebuggerF represents a debugger for the Sprockell system
 --
@@ -22,20 +23,23 @@ noDebugger = (noDebuggerF,())
 --   this creates a 'Debugger' that prints the 'SystemState'
 --   at each time step using the supplied show function.
 debuggerSimplePrint :: (SystemState -> String) -> Debugger ()
-debuggerSimplePrint showF = (dbg, ())
-    where
-        dbg dbgSt sysSt = do
-            putStrLn $ showF sysSt
-            return (dbgSt,sysSt)
+debuggerSimplePrint showF = debuggerPrintCondWaitCond showF (const True) (const False)
 
 -- | Like 'debuggerSimplePrint',
 --   but pauses execution after each time step
 --   and waits for you to hit enter to continue.
 --   It let you step through the execution slowly.
 debuggerSimplePrintAndWait :: (SystemState -> String) -> Debugger ()
-debuggerSimplePrintAndWait showF = (dbg, ())
+debuggerSimplePrintAndWait showF = debuggerPrintCondWaitCond showF (const True) (const True)
+
+
+debuggerPrintCondWaitCond :: (SystemState -> String) -- ^ show function used to show (parts of) the state
+                          -> (SystemState -> Bool)   -- ^ when this condition is True the state is printed
+                          -> (SystemState -> Bool)   -- ^ when this condition is True execution pauses
+                          -> Debugger ()
+debuggerPrintCondWaitCond showF showCond waitCond = (dbg, ())
     where
         dbg dbgSt sysSt = do
-            putStrLn $ showF sysSt
-            getLine -- wait for enter key
+            when (showCond sysSt) (putStrLn $ showF sysSt)
+            when (waitCond sysSt) (void getLine) -- wait for enter key
             return (dbgSt,sysSt)
