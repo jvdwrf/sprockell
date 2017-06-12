43478d8d07298a48ff8ada4743afce75e4114d75
Jan Kuper <j.kuper@utwente.nl>
2017-06-12 17:26:06 +0200

    Final details

--- src/DemoCharIO.hs
+++ src/DemoCharIO.hs
@@ -11,20 +11,20 @@ import Data.Char
 prog :: [Instruction]
 prog =
     writeString "What is your name? " ++
-    [ Load (ImmValue $ ord '\n') regE  -- newline terminates input
+    [ Load (ImmValue $ ord '\n') regE   -- ASCII code newline in regE for later reference
 
     -- "beginInputLoop": 39
-    , ReadInstr charIO                 -- Request a character from stdin
-    , Receive regA                     -- Save it in regA
+    , ReadInstr charIO                  -- Request a character from stdin
+    , Receive regA                      -- Save it in regA (as ASCII code)
     , Branch regA (Rel 2)
-    , Jump (Rel (-3))                  -- got 0, no input available, try again
+    , Jump (Rel (-3))                   -- got 0, no input available, try again
 
     -- got input char
-    , Compute Equal regA regE regC  -- check if it's a newline
-    , Branch regC (Abs 48)          -- then jump to "inputDone"
-    , Store regA (IndAddr regB)     -- else store character in local memory
+    , Compute Equal regA regE regC      -- check if it's a newline (remember: in regE)
+    , Branch regC (Abs 48)              -- then jump to "inputDone"
+    , Store regA (IndAddr regB)         -- else store character in local memory
     , Compute Incr regB regB regB
-    , Jump (Abs 39) -- "beginInputLoop"
+    , Jump (Abs 39)                     -- "beginInputLoop"
     ]
     -- "inputDone": 48
     ++ writeString "Hello "
@@ -34,7 +34,7 @@ prog =
     , WriteInstr regA charIO
     , Compute Incr regD regD regD
     , Compute NEq regB regD regC
-    , Branch regC (Rel (-4)) -- target "loopOutput"
+    , Branch regC (Rel (-4))            -- target "loopOutput"
     ]
     ++ writeString "!\n"
     ++ [EndProg]
