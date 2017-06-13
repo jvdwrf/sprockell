b76bff80ce81ad5717dfd5900ab3e6388553a87e
Jan Kuper <j.kuper@utwente.nl>
2017-06-13 10:44:33 +0200

    Add debugging examples

--- src/DemoCharIO.hs
+++ src/DemoCharIO.hs
@@ -21,12 +21,12 @@ prog =
 
     -- got input char
     , Compute Equal regA regE regC      -- check if it's a newline (remember: in regE)
-    , Branch regC (Abs 48)              -- then jump to "inputDone"
+    , Branch regC (Rel 4)               -- then jump to "inputDone"
     , Store regA (IndAddr regB)         -- else store character in local memory
     , Compute Incr regB regB regB
-    , Jump (Abs 39)                     -- target "beginInputLoop"
+    , Jump (Rel (-8))                   -- "beginInputLoop"
     ]
-    -- "inputDone": 48
+    -- "inputDone"
     ++ writeString "Hello "
     ++
     -- "beginLoopOutput"
@@ -42,7 +42,7 @@ prog =
 
 -- | Generate code to print a (Haskell) String
 writeString :: String -> [Instruction]
-writeString str = concat [writeChar c | c <- str]
+writeString str = concat $ map writeChar str
 
 -- | Generate code to print a single character
 writeChar :: Char -> [Instruction]
@@ -52,4 +52,30 @@ writeChar c =
     ]
 
 
+
+-- =====================================================================
+-- examples for running with/without debug information
+-- =====================================================================
+
+-- main: run without debugger
 main = run [prog]
+
+-- main_0: no debug information
+main_0 = runWithDebugger noDebugger [prog]
+
+
+-- main_1: shows all intermediate local states
+main_1 = runWithDebugger (debuggerSimplePrint showLocalMem) [prog]
+
+showLocalMem :: DbgInput -> String
+showLocalMem ( _ , systemState ) = show $ localMem $ head $ sprStates systemState
+
+
+-- main_2: shows local state only in case the sprockell writes to it
+main_2 = runWithDebugger (debuggerPrintCondWaitCond showLocalMem doesLocalMemWrite never) [prog]
+
+doesLocalMemWrite :: DbgInput -> Bool
+doesLocalMemWrite (instrs,st) = any isStoreInstr instrs
+    where
+        isStoreInstr (Store _ _) = True
+        isStoreInstr _           = False
